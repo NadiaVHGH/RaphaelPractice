d6e8ef727feb1886e25ea7cc7d614dfc4c24c20b
Nadezhda Von Holt <nadezhdavonholt@gmail.com>
2022-08-31 18:08:12 -0400

    Nadias practice is created again

--- src/TaskOne/NadiasPractice.java
+++ src/TaskOne/NadiasPractice.java
@@ -4,6 +4,8 @@ public class NadiasPractice {
     public static void main(String[] args) {
         int y = 8;
         int t = 10;
-        //this is group practice
+        //this is group practice.
+
+        //jflwjrg
     }
 }
